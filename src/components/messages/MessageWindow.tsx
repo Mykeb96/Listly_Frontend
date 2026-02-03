f6ee0a3694afc5ad7b8f1217b8a3ab1c64aa41a8
Mykael Barnes <mykael.barnes@hotmail.com>
2026-02-02 19:49:18 -0800

    feat(components): created MessageWindow component with styles

---
+++ src/components/messages/MessageWindow.tsx
@@ -0,0 +1,10 @@
+import styles from './MessagesWindow.module.scss'
+
+export default function MessageWindow() {
+
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
