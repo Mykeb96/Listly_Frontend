b589531d5c45e0f00d52fc132c2d830c7be03e54
Mykael Barnes <mykael.barnes@hotmail.com>
2026-02-09 21:09:33 -0800

    feat(pages): added faq page

---
+++ src/pages/faq/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/pages/faq/FaqPage.module.scss'
+
+export default function FaqPage(){
+
+
+    return (
+        <div className={styles.container}>
+
+        </div>
+    )
+}
