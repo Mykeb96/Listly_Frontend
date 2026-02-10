3e6b94662e0b8da6618fd3111c61a252dffa6403
Mykael Barnes <mykael.barnes@hotmail.com>
2026-02-10 15:25:04 -0800

    feat(pages): added support page

---
+++ src/pages/support/index.tsx
@@ -0,0 +1,10 @@
+import styles from '../../styles/pages/support/SupportPage.module.scss'
+
+export default function SupportPage() {
+
+
+    return (
+        <div className={styles.container}>
+        </div>
+    )
+}
