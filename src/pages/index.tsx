8050281c53114055c929957b14f50b5a77c0fc74
Mykael Barnes <mykael.barnes@hotmail.com>
2026-03-03 19:39:01 -0800

    feat(home): added state selector

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import styles from "../styles/pages/home/Home.module.scss"
+
+import { states } from "../data/States"
 
 export default function Home() {
   return (
@@ -9,6 +12,15 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <div className={styles.Container}>
+        <div className={styles.State_Selector_Container}>
+          <h2 className={styles.State_Selector_Header}>Select Your State</h2>
+          <select className={styles.State_Selector}>
+            {states.map((state, index) => <option value={state} key={index}>{state}</option>)}
+          </select>
+        </div>
+      </div>
     </>
   );
 }
