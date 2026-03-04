b8cac07b5f1eb1a44da194aab19a63276956707d
Mykael Barnes <mykael.barnes@hotmail.com>
2026-03-03 20:57:02 -0800

    feat(nav): categories now redirect to their proper URL for the listings

--- src/components/categories/CategorySection.tsx
+++ src/components/categories/CategorySection.tsx
@@ -1,13 +1,22 @@
 import styles from './CategorySection.module.scss'
+import { useRouter } from 'next/router'
 
 import { tempCategories } from './tempCategories'
 
 export default function CategorySection() {
+    const router = useRouter()
+    const { state } = router.query
 
+    function handleNav(category: string) {
+        if (!category) return
+        router.push(`/${state}/${category}`)
+    }
 
     return (
         <div className={styles.container}>
-            {tempCategories.map((category, index) => <span className={styles.category}>{category}</span>)}
+            {tempCategories.map((category, index) => 
+                <span onClick={() => handleNav(category)} className={styles.category}>{category}</span>
+            )}
         </div>
     )
 }
