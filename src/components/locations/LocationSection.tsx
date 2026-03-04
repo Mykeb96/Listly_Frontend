303be9b8868016fd17ff1bc5a132e60a5b795fdc
Mykael Barnes <mykael.barnes@hotmail.com>
2026-03-03 20:47:47 -0800

    feat(components): finished out location selector component

--- src/components/locations/LocationSection.tsx
+++ src/components/locations/LocationSection.tsx
@@ -1,11 +1,23 @@
 import styles from './LocationSection.module.scss'
+import { states } from '../../data/States'
+
+import { useRouter } from 'next/router'
 
 export default function LocationSection() {
+    const router = useRouter()
+
+    function handleNav(state: string) {
+        if (!state) return
+        router.push(`/${state}`)
+    }
 
 
     return (
         <div className={styles.container}>
-            Locations
+            <h4 className={styles.Header}>Change your location</h4>
+            <ul className={styles.State_List}>
+                {states.map((state, index) => <li key={index} className={styles.State} onClick={() => handleNav(state)}>{state}</li>)}
+            </ul>
         </div>
     )
 }
